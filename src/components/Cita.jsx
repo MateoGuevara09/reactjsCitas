36f574a2f3b509fe08581f82169a0da47f0e1a82
MateoGuevara09 <97854957+MateoGuevara09@users.noreply.github.com>
2023-06-05 10:22:57 -0300

    terminado

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -1,13 +1,19 @@
 
 const Cita =  (props) => {
+    const eliminar = () => {
+        const nuevoArray = [...props.cita]
+        nuevoArray.splice(props.punto,1)
+        props.setcitas(nuevoArray)
+    }
+
     return (
-        <div className="cita" id={props.Mascota} display= "">
+        <div className="cita" display= "">
             <p>Mascota: <span>{props.Mascota}</span></p>
             <p>Dueño: <span>{props.Dueño}</span></p>
             <p>Fecha: <span>{props.Fecha}</span></p>
             <p>Hora: <span>{props.Hora}</span></p>
             <p>Sintomas: <span>{props.Sintomas}</span></p>
-            <button className="button elimnar u-full-width">Eliminar ×</button>
+            <button className="button elimnar u-full-width" onClick={eliminar}>Eliminar ×</button>
         </div>
     )
 }
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Cita from "./Cita";
 const Listado = (props) => {
-    console.log(props.citas)
     return(
     <div className="container">
-        {props.citas.map((cita, i) => <Cita key={i} {...cita}/>)}
+        {props.citas.map((cita, i) => <Cita key={i} {...cita} setcitas={props.setCitas} cita={props.citas} punto={i}/>)}
     </div>
     )
 };
